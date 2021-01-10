51423c70c6a1508bd62258915c83b941db439d70
yerson <yerson.a06@gmail.com>
2021-01-09 20:50:16 -0400

    verifying controllers

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,31 +1,37 @@
 const db = require("../models");
 
-// Defining methods for the bookController
+// Defining methods for the booksController
 module.exports = {
   findAll: function(req, res) {
-    db.Book.find(req.query)
-      .then(dbBook => res.json(dbBook))
+    db.Book
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.Book.findById(req.params.id)
-      .then(dbBook => res.json(dbBook))
+    db.Book
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.Book.create(req.body)
-      .then(dbBook => res.json(dbBook))
+    db.Book
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Book.findOneAndUpdate({ id: req.params.id }, req.body)
-      .then(dbBook => res.json(dbBook))
+    db.Book
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Book.findById(req.params.id)
-      .then(dbBook => dbBook.remove())
-      .then(dbBook => res.json(dbBook))
+    db.Book
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   }
 };
