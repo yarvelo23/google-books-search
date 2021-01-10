7d65cc45c49c40c20f829fe66dc2546a921c6196
yerson <yerson.a06@gmail.com>
2021-01-09 20:54:54 -0400

    backend working

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,16 +1,16 @@
 const router = require("express").Router();
-const bookController = require("../../controllers/bookController");
+const booksController = require("../../controllers/booksController");
 
 // Matches with "/api/books"
 router.route("/")
-  .get(bookController.findAll)
-  .post(bookController.create);
+  .get(booksController.findAll)
+  .post(booksController.create);
 
 // Matches with "/api/books/:id"
 router
   .route("/:id")
-  .get(bookController.findById)
-  .put(bookController.update)
-  .delete(bookController.remove);
+  .get(booksController.findById)
+  .put(booksController.update)
+  .delete(booksController.remove);
 
 module.exports = router;
